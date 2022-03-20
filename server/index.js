c9e0ada7ff6ef0b8f29ba893fe6b6b7c768ff293
tuurGevers <tuur.gevers@hotmail.com>
2022-03-20 17:50:33 +0100

    initial

--- server/index.js
+++ server/index.js
@@ -14,7 +14,9 @@ const db = mysql.createPool({
 app.use(cors())
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
+
 app.get("/", (req, res) => {
+    res.render("hello")
 });
 
 app.post("/api/insert", (req, res) => {
